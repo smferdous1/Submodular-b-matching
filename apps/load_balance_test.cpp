53b504f5db919c88b19b917a2b5ab07ebe83b480
S M Ferdous <sferdou@bell-fe03.rcac.purdue.edu>
2021-02-08 19:27:14 -0500

    removing unnecessary  codes

--- apps/load_balance_test.cpp
+++ apps/load_balance_test.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<iomanip>
 #include<cstdlib>
-
+#include<fstream>
 #include "graph.h"
 #include "matching.h"
 #include "input.h"
@@ -11,18 +11,17 @@ using namespace std;
 int main(int argc,char *argv[])
 {
 
-    if(argc != 6)
+    if(argc != 5)
     {
-       cout<< "usage: ./progr_name filename b nrun alpha nMachine"<<endl; 
+       cout<< "usage: ./progr_name filename outfile alpha nMachine"<<endl; 
        exit(1);
     }
     Input graphInput;
     
     std::string fileName = argv[1];
-    int b = atoi(argv[2]);
-    int nRun = atoi(argv[3]);
-    float alpha = atof(argv[4]);
-    int nMachine = atoi(argv[5]);
+    std::string outfile = argv[2];
+    float alpha = atof(argv[3]);
+    int nMachine = atoi(argv[4]);
 
 
     LightGraph G;
@@ -75,29 +74,30 @@ int main(int argc,char *argv[])
 
     double totalTime = 0;
     int nPartition = 1;
-    for(int i=0;i<nRun;i++)
-    {
-        currentTime(startTime);
-        submodularGreedybMatching(G,cV,bV,alpha,nPartition,cW,outPut,totalWeight,matchingSize); 
-        //localLazyGreedy(G,cV,bV,alpha,nPartition,cW,outPut,totalWeight,matchingSize); 
-        currentTime(endTime);
-        totalTime = totalTime + endTime-startTime;
-    }
-    for(NODE_T i=nRow;i<n;i++)
-    {
-        cout<<cW[i]<<endl; 
-    }
-    cout<<"%%%"<<endl;
+
+    currentTime(startTime);
+    submodularGreedybMatching(G,cV,bV,alpha,nPartition,cW,outPut,totalWeight,matchingSize); 
+    //localLazyGreedy(G,cV,bV,alpha,nPartition,cW,outPut,totalWeight,matchingSize); 
+    currentTime(endTime);
+    totalTime = totalTime + endTime-startTime;
+
+    //open the output file and write down the allocation
+    ofstream write_match;
+    write_match.open(outfile);
+    write_match<<"# of jobs: "<<nRow<<endl;
+    write_match<<"# of machines: "<<nMachine<<endl;
+    write_match<<"#job_id,machine_id,load"<<endl;
     for(NODE_T i =0;i<outPut.size();i++)
     {
         //job" "machine
-        cout<<outPut[i].e.u<<" "<<outPut[i].e.v<<endl;
+        write_match<<outPut[i].e.u<<","<<outPut[i].e.v-nRow<<","<<outPut[i].weight<<endl;
+
     }
 
     //cout << std::fixed<<std::setprecision(0)<<totalWeight<< " "<<matchingSize<<" "<<b*n/2-matchingSize<<" ";
     //cout<< n <<","<<m<<","<<std::fixed<<std::setprecision(5)<<totalWeight<< ","<<matchingSize<<","<<b*n/2-matchingSize<<",";
 
-    cout<< std::setprecision(4)<<2.0*matchingSize/n<<","<<totalTime/nRun<<endl;
+    cout<< std::setprecision(4)<<2.0*matchingSize/n<<","<<totalTime<<endl;
 
     return 0;
 }
